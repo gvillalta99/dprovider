c8a0a19b1a73c5477e6b0c25e10b5da6528187c5
gvillalta99 <gvillalta99@gmail.com>
2016-06-06 14:10:44 -0300

    Basic validation on Message#title
    Validates the presence of a message title

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:message)).to be_valid
+  end
+  it 'requires the presence of a title' do
+    expect(FactoryGirl.build(:message, title: '')).to be_invalid
+  end
 end
